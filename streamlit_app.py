6023401e3b9504c7f8493c87a6ab8d198bcc7e7b
ilenaGkova <145449700+ilenaGkova@users.noreply.github.com>
2025-01-07 11:46:25 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,41 +1,40 @@
 import streamlit as st
-from pymongo import MongoClient
+from mongo_connection import get_database, insert_starter_data
 
-host = "localhost"  # The host part of the MongoDB URI
-port = 27017  # The port number, extracted from the URI
-username = "E20030"  # Replace with your MongoDB username
-password = "E20030"  # Replace with your MongoDB password
+data = get_database
+insert_data = insert_starter_data
 
-# Define Starter Data
-Users = [
-    {'name': 'Ilena',
-     'surname': 'Gkova',
-     'username': 'Admin',
-     'passowrd': 'Admin123',
-     'repeat_preference': 5,
-     'age_category': '18-25',
-     'level': 1,
-     'score': 500,
-     'streak': 0,
-     'days_summed': 0,   
-     'status': 2, 
-     'role': 'Admin'},
-]
+def log_in_user(username, password):
+    query = {
+        'username': username,
+        'password': password
+    }
+    user_collection = data['StressUser']
+    if user_collection.count_documents(query) == 0:
+        st.sidebar.write('You do not have an account')
+    else:
+        st.sidebar.write('You have an account')
+    return False
 
-def get_database():
-    client = client = MongoClient(host=host, port=port, username=username, password=password)
-    return client.StressTest
+st.set_page_config(
+    page_title="Log In",
+    page_icon="👋",
+)
 
-def insert_starter_data():
-    data = get_database
-    collection = data['StressUser']
-    for entries in Users:
-        if collection.count_documents({'username': entries['username']}) == 0:
-            print(f"Inserting data for user: {entries['username']}...")
-            try:
-                data['StressUser'].insert_one(data)
-            except Exception as e:
-                print(f"Error inserting data: {e}")
-        else:
-            print(f"Data for user: {data['username']} already exists. Skipping insert.")
-    return True
+st.sidebar.write ('Already have an account? Sign it!')
+username = st.sidebar.text_input("Your Username", key="username")
+password = st.sidebar.text_input("Your Password", key="password")
+st.sidebar.button('Log in', on_click=log_in_user, args=[username, password])
+
+"""
+# Wellcome to Stress Test!
+Please answer the following questions and we'll create your account
+"""
+st.text_input("What's your first name?", key="first_name")
+st.text_input("What's your last name?", key="last_name")
+st.text_input("What's your username?", key="user_username")
+st.text_input("What's your password?", key="user_password")
+chosen = st.radio(
+        'Age',
+        ("18-25", "26-35", "36-55", "56-70", "70+"))
+st.button('Let us get started')
