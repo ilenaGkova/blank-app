f7c519710b07de168941a6b07ec5a44737be0a72
ilenaGkova <145449700+ilenaGkova@users.noreply.github.com>
2025-01-08 20:14:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,28 @@
 import streamlit as st
-from mongo_connection import get_database, insert_starter_data
+from mongo_connection import get_database
 
-data = get_database
-insert_data = insert_starter_data
+st.set_page_config(
+    page_title="Log In",
+    page_icon="👋",
+)
 
 def log_in_user(username, password):
     query = {
         'username': username,
         'password': password
     }
-    user_collection = data['StressUser']
-    if user_collection.count_documents(query) == 0:
-        st.sidebar.write('You do not have an account')
+    data = get_database()
+    if data is not None:
+        user_collection = data['StressApp']['User']
+        if user_collection.count_documents(query) == 0:
+            st.sidebar.write('You do not have an account')
+        else:
+            st.sidebar.write('You have an account')
+        return True
     else:
-        st.sidebar.write('You have an account')
-    return False
-
-st.set_page_config(
-    page_title="Log In",
-    page_icon="👋",
-)
+        st.error("Database connection not established. Please check your connection settings.")
+        st.sidebar.write('No database connection - try again later')
+        return False
 
 st.sidebar.write ('Already have an account? Sign it!')
 username = st.sidebar.text_input("Your Username", key="username")
@@ -38,3 +41,4 @@ chosen = st.radio(
         'Age',
         ("18-25", "26-35", "36-55", "56-70", "70+"))
 st.button('Let us get started')
+
