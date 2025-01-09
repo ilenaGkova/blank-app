a9cc15b53388f0552fab379be34d8163c5a99824
ilenaGkova <145449700+ilenaGkova@users.noreply.github.com>
2025-01-09 16:42:15 +0200

    Update mongo_connection.py

--- mongo_connection.py
+++ mongo_connection.py
@@ -10,6 +10,7 @@ def init_connection():
         # Ping the server to check the connection
         client.admin.command('ping')
         st.success("Connected to MongoDB!")
+        condition = insert_starter_data()
         return client
     except Exception as e:
         st.error(f"Error connecting to MongoDB: {e}")
@@ -33,20 +34,34 @@ Users = [
      'role': 'Admin'},
 ]
 
-@st.cache_resource
-def get_database():
-    return client
-
 def insert_starter_data():
-    data = get_database()
-    collection = data['StressApp']['User']
-    for entries in Users:
-        if collection.count_documents({'username': entries['username']}) == 0:
-            print(f"Inserting data for user: {entries['username']}...")
-            try:
-                data['User'].insert_one(entries)
-            except Exception as e:
-                print(f"Error inserting data: {e}")
+    data = init_connection()
+    if data is not None:
+        collection = data['StressApp']['User']
+        st.sidebar.write(collection)
+        for entries in Users:
+            if collection.count_documents({'username': entries['username']}) == 0:
+                try:
+                    data['User'].insert_one(entries)
+                except Exception as e:
+                    print(f"Error inserting data: {e}") 
+    else:
+        st.error("Database connection not established. Unable to create entries.")   
+    return True
+
+def validate_user(username, password):
+    query = {
+        'username': username,
+        'password': password
+    }
+    data = init_connection()
+    if data is not None:
+        user_collection = data['StressApp']['User']
+        if user_collection.count_documents(query) == 0:
+            return 1
         else:
-            print(f"Data for user: {entries['username']} already exists. Skipping insert.")
+            return 2
+    else:
+        st.error("Database connection not established. Please check your connection settings.")
+        return 3
 
