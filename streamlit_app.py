153d601ae1e6983282d5ca50ceaea455421f0ad0
ilenaGkova <145449700+ilenaGkova@users.noreply.github.com>
2025-01-11 20:59:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from mongo_connection import get_database
+from mongo_connection import validate_user, new_user
 
 st.set_page_config(
     page_title="Log In",
@@ -7,10 +7,12 @@ st.set_page_config(
 )
 
 def log_in_user(username, password):
-    if validate_user(username, password):
-        st.sidebar.write('You have an account')
-    else:
-        st.sidebar.write('You do not have an account')
+    move_on, message = validate_user(username, password)
+    st.sidebar.write(message)
+
+def create_user(first_name,last_name,username,password,chosen):
+    move_on, message = new_user(first_name,last_name,username,password,chosen)
+    st.write(message)
 
 st.sidebar.write ('Already have an account? Sign it!')
 username = st.sidebar.text_input("Your Username", key="username")
@@ -28,5 +30,5 @@ st.text_input("What's your password?", key="user_password")
 chosen = st.radio(
         'Age',
         ("18-25", "26-35", "36-55", "56-70", "70+"))
-st.button('Let us get started')
+st.button('Let us get started', on_click=create_user, args=[first_name,last_name,username,password,chosen])
 
