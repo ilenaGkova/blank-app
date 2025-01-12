b2c375648033f1c38f5b3cacaa0a9342cd8c048b
ilenaGkova <145449700+ilenaGkova@users.noreply.github.com>
2025-01-12 19:40:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from mongo_connection import validate_user, new_user
+from mongo_connection import validate_user, new_user, record_question
 
 st.set_page_config(
     page_title="Log In",
@@ -9,21 +9,16 @@ st.set_page_config(
 if "menu" not in st.session_state:
     st.session_state.menu = False
 
-def set_username (username):
-    if "current_username" not in st.session_state:
-        st.session_state.current_username = username
+if "current_username" not in st.session_state:
+    st.session_state.current_username = "username"
+
+st.sidebar.write ('Already have an account? Sign it!')
 
 def log_in_user(username, password):
     move_on, message = validate_user(username, password)
     st.session_state.menu = move_on
     st.sidebar.write(message)
 
-def create_user(first_name,last_name,user_username,user_password,chosen):
-    move_on, message = new_user(first_name,last_name,user_username,user_password,chosen)
-    st.session_state.menu = move_on
-    st.write(message)
-
-st.sidebar.write ('Already have an account? Sign it!')
 username = st.sidebar.text_input("Your Username", key="username")
 password = st.sidebar.text_input("Your Password", key="password")
 st.sidebar.button('Log in', on_click=log_in_user, args=[username, password])
@@ -32,12 +27,30 @@ st.sidebar.button('Log in', on_click=log_in_user, args=[username, password])
 # Wellcome to Stress Test!
 Please answer the following questions and we'll create your account
 """
-first_name = st.text_input("What's your first name?", key="first_name")
-last_name = st.text_input("What's your last name?", key="last_name")
-user_username = st.text_input("What's your username?", key="user_username")
-user_passname = st.text_input("What's your password?", key="user_password")
-chosen = st.radio('Age',("18-25", "26-35", "36-55", "56-70", "70+"))
-st.button('Let us get started', on_click=create_user, args=[first_name,last_name,username,password,chosen])
+
+question_first_name = "What's your first name?"
+question_last_name = "What's your last name?"
+question_username = "What's your username?"
+question_password = "What's your password?"
+question_age = "Age"
+
+def create_user(first_name,last_name,user_username,user_password,age):
+    move_on, message = new_user(first_name,last_name,user_username,user_password,age)
+    st.session_state.menu = move_on
+    if move_on:
+        record_question(question_first_name,first_name,user_username)
+        record_question(question_last_name,last_name,user_username)
+        record_question(question_username,user_username,user_username)
+        record_question(question_password,user_password,user_username)
+        record_question(question_age,age,user_username)
+    st.write(message)
+
+first_name = st.text_input(question_first_name, key="first_name")
+last_name = st.text_input(question_last_name, key="last_name")
+user_username = st.text_input(question_username, key="user_username")
+user_passname = st.text_input(question_password, key="user_password")
+age = st.radio(question_age,("18-25", "26-35", "36-55", "56-70", "70+"))
+st.button('Let us get started', on_click=create_user, args=[first_name,last_name,username,password,age])
 
 
 start_page = st.Page("streamlit_app.py", title="Sign Me In")
@@ -47,3 +60,5 @@ if st.session_state.menu:
 else:
     pg =st.navigation([start_page, status_page], position="hidden")
 
+def set_username (username):
+    st.session_state.current_username = username
