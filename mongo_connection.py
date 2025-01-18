00a4c06d33351f98cfe32ebe83e61bf84d24966d
ilenaGkova <145449700+ilenaGkova@users.noreply.github.com>
2025-01-18 18:40:47 +0200

    Update mongo_connection.py

--- mongo_connection.py
+++ mongo_connection.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+import random
 from Tables import Users, Questions, Status, Favorite, Recommendations_Per_Person, Recommendations, Tags
 
 def validate_user(username, password):
@@ -19,25 +20,25 @@ def new_user(first_name,last_name,username,password,age):
         valid_username = True
         valid_password = True
         index = 0
-        while index < len(Users) and valid_username and valid_password:
-            valid_username = not Users[index]["username"] == username
-            valid_password = not Users[index]["password"] == password
+        while index <= len(Users) - 1 and valid_username and valid_password:
+            valid_username = not Users[index]['Username'] == username
+            valid_password = not Users[index]['Password'] == password
             index += 1  
         if valid_username and valid_password:
             new_entry = [
                 {
-                    'name': first_name,
-                    'surname': last_name,
-                    'username': username,
-                    'password': password,
-                    'repeat_preference': 1,
-                    'age_category': age,
-                    'level': 0,
-                    'score': 0,
-                    'streak': 0,
-                    'days_summed': 0,   
-                    'status': 1, 
-                    'role': 'User',
+                    'Name': first_name,
+                    'Surname': last_name,
+                    'Username': username,
+                    'Password': password,
+                    'Repeat_Prefence': 1,
+                    'Age_Category': age,
+                    'Level': 0,
+                    'Score': 0,
+                    'Streak': 0,
+                    'Days_Summed': 0,   
+                    'Status': 1, 
+                    'Role': 'User',
                     'Created_At': datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                 }
             ]
@@ -71,6 +72,7 @@ def record_status(username,focus_area,stress_level,time_available,suggestions):
                 'Focus_Area': focus_area,
                 'Stress_Level': stress_level,
                 'Suggestions': suggestions,
+                'Time_Available': time_available,
                 'Created_At': datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             }
         ]
@@ -90,3 +92,87 @@ def get_past_recomendations(username,days_behind):
         (Recommendations_Per_Person["created_at"] >= start_of_range) & 
         (Recommendations_Per_Person["created_at"] <= end_of_today) & 
         (Recommendations_Per_Person["user_username"] == username)
+    ]
+    return user_past_recomendations
+
+def get_status(username):
+    index = len(Status) - 1
+    found = False
+    while index >= 0 and not found:
+        found = Status[index]['Username'] == username
+        index -= 1
+    last_status = None
+    time_diff = None
+    if found:
+        last_status = datetime.strptime(Status[index+1]['Created_At'].to_datetime,'%Y-%m-%d %H:%M:%S')
+        current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        time_diff = current_datetime - last_status
+        return time_diff <= timedelta(hours=24), index
+    index = len(Status) - 1
+    found = False
+    while index >= 0 and not found:
+        found = Users[index]['Username'] == username
+        index -= 1
+    if found:
+        return True, index 
+    return False, -1 
+
+def get_recomendations(username):
+    index = 0
+    found = False
+    age = None
+    while index <= len(Users) - 1 and not found:
+        found = Users[index]['Username'] == username
+        index += 1
+    user_recomendations = []
+    if found:
+        user_past_recomendations = get_past_recomendations(username,Users[index-1]['Repeat_Prefence'])
+        age = Users[index-1]['Age_Category']
+        condition, index = get_status(username)
+        focus_area = Status[index]['Focus_Area']
+        stress_level = Status[index]['Stress_Level']
+        time_available = Status[index]['Time_Available']
+        suggestions = Status[index]['Suggestions']
+        index = 1
+        while index <= suggestions:
+            pottential_recomendation_index = random.randint(1, len(Recommendations))
+            valid = True
+            table_filtered = [(user_past_recomendations[pottential_recomendation_index]['ID'] == pottential_recomendation_index)]  
+            for entry in table_filtered:
+                valid = False
+            if valid or fails == 3:
+                table_filtered = [(user_recomendations[pottential_recomendation_index]['ID'] == pottential_recomendation_index)]
+                for entry in table_filtered:
+                    valid = False
+                if valid or fails == 3:
+                    table_filtered = [(Tags[pottential_recomendation_index]['ID'] == pottential_recomendation_index)]
+                    for entry in table_filtered:
+                        if entry['Title_Of_Criteria'] == 'Age Variant':
+                            valid = entry['Category'] == age
+                        elif entry['Title_Of_Criteria'] == 'Focus Area':
+                            valid = entry['Category'] == focus_area
+                        elif entry['Title_Of_Criteria'] == 'Stress Level':
+                            valid = entry['Category'] == stress_level
+                        else:
+                            valid = entry['Category'] == time_available
+                    if valid or fails == 3:
+                        new_entry = [
+                            {
+                                'Username': username,
+                                'ID': pottential_recomendation_index,
+                                'Outcome': None,
+                                'Created_At': datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                            }
+                        ]
+                        user_recomendations.append(new_entry)
+                        index += 1
+                        fails = 0
+                    else:
+                        fails += 1
+                else:
+                    fails += 1
+            else:
+                fails += 1
+        Recommendations_Per_Person.append(user_recomendations)
+        return True, user_recomendations
+    return False, user_recomendations
