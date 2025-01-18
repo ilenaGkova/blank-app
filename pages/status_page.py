6d6a40950e2a1ea82b5e875bfaa4b3c98e23895f
ilenaGkova <145449700+ilenaGkova@users.noreply.github.com>
2025-01-18 17:20:55 +0200

    Update status_page.py

--- pages/status_page.py
+++ pages/status_page.py
@@ -30,7 +30,7 @@ def make_status(focus_area,stress_level,time_available,suggestions):
 
 focus_area = st.radio(question_focus_area,("Work/Career", "Finances", "Health & Well-being", "Relationships", "Time Management", "Personal Identity", "Major Life Changes", "Social Media & Technology", "Uncertainty & Future Planning"))
 stress_level = st.number_input(question_stress_level, min_value=min_limit, max_value=max_limit)
-time_available = st.number_input(question_time_available, min_value=min_limit+2, max_value=max_limit)
+time_available = st.number_input(question_time_available, min_value=min_limit+2, max_value=2*max_limit)
 suggestions = st.number_input(question_suggestions, min_value=min_limit, max_value=max_limit)
 status_button = st.button('Let us get started', on_click=make_status, args=[focus_area,stress_level,time_available,suggestions])
 
