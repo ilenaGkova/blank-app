bea6503f436fb7c326816c9a1627d7b1c14c3760
ilenaGkova <145449700+ilenaGkova@users.noreply.github.com>
2025-02-02 20:00:45 +0200

    Update status_page.py

--- pages/status_page.py
+++ pages/status_page.py
@@ -26,14 +26,14 @@ question_time_available = "How much time are you willing to spend in reducing st
 question_suggestions = "How many suggestions do you want?"
 
 def make_status(focus_area,stress_level,time_available,suggestions):
-    if not st.session_state.current_username == "username":
-        move_on, message = record_status(st.session_state.username,focus_area,stress_level,time_available,suggestions)
+    if not index == -1:
+        move_on, message = record_status(st.session_state.current_username,focus_area,stress_level,time_available,suggestions)
         st.session_state.menu = move_on
         if move_on:
-            record_question(question_focus_area,focus_area,st.session_state.username)
-            record_question(question_stress_level,stress_level,st.session_state.username)
-            record_question(question_suggestions,suggestions,st.session_state.username)
-            record_question(question_time_available,time_available,st.session_state.username)
+            record_question(question_focus_area,focus_area,st.session_state.current_username)
+            record_question(question_stress_level,stress_level,st.session_state.current_username)
+            record_question(question_suggestions,suggestions,st.session_state.current_username)
+            record_question(question_time_available,time_available,st.session_state.current_username)
         st.sidebar.write(message)
 
 focus_area = st.radio(question_focus_area,("Work/Career", "Finances", "Health & Well-being", "Relationships", "Time Management", "Personal Identity", "Major Life Changes", "Social Media & Technology", "Uncertainty & Future Planning"))
@@ -46,7 +46,7 @@ if st.session_state.menu and status_button and not index == -1:
     st.switch_page("pages/main.py")
 
 if not index == -1:
-    st.sidebar.write(update_user_streak(st.session_state.current_username))
+    st.sidebar.write(update_user_streak(Users[index]['Username']))
     st.sidebar.write('Name:', Users[index]['Name'])
     st.sidebar.write('Surname:', Users[index]['Surname'])
     st.sidebar.write('Level:', Users[index]['Level'])
