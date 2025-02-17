84d989dddbf2d44434698b3e1dd18b628cd73eff
ilenaGkova <145449700+ilenaGkova@users.noreply.github.com>
2025-02-17 17:02:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,65 +1,62 @@
 import streamlit as st
-from mongo_connection import get_status, validate_user, new_user, record_question
-from Tables import Questions
 
 st.set_page_config(
     page_title="Log In",
     page_icon="👋",
 )
 
+from mongo_connection import generate_unique_passcode, get_status, validate_user, new_user, record_question
+from faker import Faker
+
 if "menu" not in st.session_state:
     st.session_state.menu = False
 
-if "current_username" not in st.session_state:
-    st.session_state.current_username = "username"
+if "current_passcode" not in st.session_state:
+    st.session_state.current_passcode = 1
 
 st.sidebar.write ('Already have an account? Sign it!')
 
-def log_in_user(username, password):
-    move_on, message = validate_user(username, password)
+question_passcode = "What's your passcode?"
+
+def log_in_user(password):
+    move_on, message = validate_user(password)
     st.session_state.menu = move_on
     if move_on:
-        set_username(username)
+        record_question(question_passcode,password,password)
+        set_username(password)
     st.sidebar.write(message)
 
-username = st.sidebar.text_input("Your Username", key="username")
-password = st.sidebar.text_input("Your Password", key="password")
-log_in_button = st.sidebar.button('Log in', on_click=log_in_user, args=[username, password])
+passcode = st.sidebar.text_input(question_passcode, key="passcode")
+log_in_button = st.sidebar.button('Log in', on_click=log_in_user, args=[passcode])
 
 """
 # Wellcome to Stress Test!
 Please answer the following questions and we'll create your account
 """
 
-question_first_name = "What's your first name?"
-question_last_name = "What's your last name?"
 question_username = "What's your username?"
-question_password = "What's your password?"
 question_age = "Age"
 
-def create_user(first_name,last_name,user_username,user_password,age):
-    move_on, message = new_user(first_name,last_name,user_username,user_password,age)
+def create_user(user_username,user_passcode,age):
+    move_on, message = new_user(user_username,user_passcode,age)
     st.session_state.menu = move_on
     if move_on:
-        record_question(question_first_name,first_name,user_username)
-        record_question(question_last_name,last_name,user_username)
         record_question(question_username,user_username,user_username)
-        record_question(question_password,user_password,user_username)
+        record_question(question_passcode,user_passcode,user_username)
         record_question(question_age,age,user_username)
-        set_username(user_username)
+        set_username(user_passcode)
     st.sidebar.write(message)
 
-first_name = st.text_input(question_first_name, key="first_name")
-last_name = st.text_input(question_last_name, key="last_name")
-user_username = st.text_input(question_username, key="user_username")
-user_password = st.text_input(question_password, key="user_password")
+fake = Faker()
+user_username = st.text_input(question_username, key="user_username", value=fake.user_name())
+user_passcode = st.text_input(question_passcode, key="user_password", value=generate_unique_passcode(), disabled=True)
 age = st.radio(question_age,("18-25", "26-35", "36-55", "56-70", "70+"))
-sign_in_button = st.button('Let us get started', on_click=create_user, args=[first_name,last_name,user_username,user_password,age])
+sign_in_button = st.button('Let us get started', on_click=create_user, args=[user_username,user_passcode,age])
 
-def set_username (username):
-    st.session_state.current_username = username 
+def set_username (passcode):
+    st.session_state.current_passcode = passcode
 
-condition, index = get_status(st.session_state.current_username)
+condition, index = get_status(st.session_state.current_passcode)
 
 if st.session_state.menu and (sign_in_button or log_in_button) and index == -1 :
     st.switch_page("pages/status_page.py")
