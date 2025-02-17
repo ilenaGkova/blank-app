cccdc824fe1d4b58d36045b50e36206b15adc2e1
ilenaGkova <145449700+ilenaGkova@users.noreply.github.com>
2025-02-17 17:03:58 +0200

    Update Tables.py

--- Tables.py
+++ Tables.py
@@ -1,5 +1,5 @@
 Users = [
-    {'Name': 'Ilena', 'Surname': 'Gkova', 'Username': 'Admin', 'Password': 'Admin123','Repeat_Preference': 5, 'Age_Category': '18-25', 'Level': 1,'Score': 500, 'Streak': 0, 'Days_Summed': 0,'Status': 0, 'Role': 'Admin','Created_At': '2025-01-18 12:00:00'}
+    {'Username': 'Admin', 'Passcode': 'Admin123','Repeat_Preference': 5, 'Age_Category': '18-25', 'Level': 1,'Score': 500, 'Streak': 0, 'Days_Summed': 0, 'Role': 'Admin','Created_At': '2025-01-18 12:00:00'}
 ]
 
 Questions = []
