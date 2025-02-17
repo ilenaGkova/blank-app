398ffb8d559b16e9d2ece1f979314376bd69d953
ilenaGkova <145449700+ilenaGkova@users.noreply.github.com>
2025-02-17 18:57:56 +0200

    Update status_page.py

--- pages/status_page.py
+++ pages/status_page.py
@@ -1,58 +1,51 @@
 import streamlit as st
-from mongo_connection import get_user, record_status, record_question, update_user_streak
-from Tables import Users
+from mongo_connection import init_connection, record_status, record_question, update_user_streak
 
 st.title("Tell us how you're doing today!")
 
 if "menu" not in st.session_state:
     st.session_state.menu = False
 
-if "current_username" not in st.session_state:
-    st.session_state.current_username = "username"
+if "current_passcode" not in st.session_state:
+    st.session_state.current_passcode = 1
 
-if not st.session_state.current_username == "username":
-    st.write("Hello", st.session_state.current_username, "Please answer the questions below")
+client = init_connection()
+db = client.StressTest
+User = db["User"]
+current_user = User.find_one({"Passcode": st.session_state.current_passcode})
+
+if not current_user == None:
+    st.write("Hello", current_user['Username'], "Please answer the questions below")
 else:
     st.write('Something went wrong, user not registered.')
 
-index = get_user(st.session_state.current_username)
-
 min_limit = 1
 max_limit = 10
-
-question_focus_area = "Where would you like to focus today?"
 question_stress_level = "How would you rate your stress level?"
-question_time_available = "How much time are you willing to spend in reducing stress today?"
-question_suggestions = "How many suggestions do you want?"
 
-def make_status(focus_area,stress_level,time_available,suggestions):
-    if not index == -1:
-        move_on, message = record_status(st.session_state.current_username,focus_area,stress_level,time_available,suggestions)
+
+def make_status(stress_level):
+    if not current_user == None:
+        move_on, message = record_status(st.session_state.current_passcode,stress_level)
         st.session_state.menu = move_on
-        if move_on:
-            record_question(question_focus_area,focus_area,st.session_state.current_username)
-            record_question(question_stress_level,stress_level,st.session_state.current_username)
-            record_question(question_suggestions,suggestions,st.session_state.current_username)
-            record_question(question_time_available,time_available,st.session_state.current_username)
+        if move_on: record_question(question_stress_level,stress_level,st.session_state.current_passcode)
         st.sidebar.write(message)
 
-focus_area = st.radio(question_focus_area,("Work/Career", "Finances", "Health & Well-being", "Relationships", "Time Management", "Personal Identity", "Major Life Changes", "Social Media & Technology", "Uncertainty & Future Planning"))
-stress_level = st.number_input(question_stress_level, min_value=min_limit, max_value=max_limit)
-time_available = st.number_input(question_time_available, min_value=min_limit+2, max_value=2*max_limit)
-suggestions = st.number_input(question_suggestions, min_value=min_limit, max_value=max_limit)
-status_button = st.button('Let us get started', on_click=make_status, args=[focus_area,stress_level,time_available,suggestions])
 
-if st.session_state.menu and status_button and not index == -1:
+stress_level = st.number_input(question_stress_level, min_value=min_limit, max_value=max_limit)
+status_button = st.button('Let us get started', on_click=make_status, args=[stress_level])
+
+if st.session_state.menu and status_button and not current_user == None:
     st.switch_page("pages/main.py")
 
-if not index == -1:
-    st.sidebar.write(update_user_streak(Users[index]['Username']))
-    st.sidebar.write('Name:', Users[index]['Name'])
-    st.sidebar.write('Surname:', Users[index]['Surname'])
-    st.sidebar.write('Level:', Users[index]['Level'])
-    st.sidebar.write('Score:', Users[index]['Score'])
-    st.sidebar.write('Days connected:', Users[index]['Days_Summed'])
-    st.sidebar.write('Streak:', Users[index]['Streak'])
-    st.sidebar.write("Don't show me the same saggestion for ", Users[index]['Repeat_Preference'], ' day(s) after') 
+if not current_user == None:
+    st.sidebar.write(update_user_streak(st.session_state.current_passcode))
+    st.sidebar.write('Username:', current_user['Username'])
+    st.sidebar.write('Focus Area:', current_user['Focus_Area'])
+    st.sidebar.write('Number of Suggestions:', current_user['Suggestions'])
+    st.sidebar.write('Time Available:', current_user['Time_Available'])
+    st.sidebar.write('Days Connected:', current_user['Days_Summed'])
+    st.sidebar.write('Streak:', current_user['Streak'])
+    st.sidebar.write("Don't show me the same saggestion for ", current_user['Repeat_Preference'], ' day(s) after') 
 else:
     st.sidebar.write('Something went wrong, user not registered.')
